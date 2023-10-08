ba4c0a358ca2082cfe87cd84107879a7987264bc
karmegamm <karmegamm@anjaconline.org>
2023-10-08 18:55:37 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-
+import { Button } from "@material-tailwind/react";
 
 function App() {
-  return(
-  <h1 className="text-3xl font-bold underline">
-    Hello world! hello
-  </h1>
-  )  
+  return <Button>Button</Button>;
+ 
 }
 
 export default App
